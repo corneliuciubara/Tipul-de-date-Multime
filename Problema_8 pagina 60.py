846e9df1db4743f110dfd0dd282b5517b949921e
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2021-11-14 16:35:46 +0200

    Add files via upload

---
+++ Problema_8 pagina 60.py
@@ -0,0 +1,8 @@
+A = {1,4,6,8}
+B = {3,4,2,6}
+print('Intersecția mulțimilor: ', A.intersection(B))
+print('Reuniunea mulțimilor: ', A.union(B))
+print('Diferența mulțimilor: ', A.difference(B))
+        
+
+
