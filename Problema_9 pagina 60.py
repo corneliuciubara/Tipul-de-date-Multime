207986bc668d8732e77df17a4b823c37d37d71a2
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2021-11-16 19:13:02 +0200

    Delete Problema_9 pagina 60.py

--- Problema_9 pagina 60.py
+++
@@ -1,5 +0,0 @@
-A = {'A','R','W','O'}
-B = {'E','F','S','K'}
-print('Intersecția mulțimilor: ', A.intersection(B))
-print('Reuniunea mulțimilor: ', A.union(B))
-print('Diferența mulțimilor: ', A.difference(B))
