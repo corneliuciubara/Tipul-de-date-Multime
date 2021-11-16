6954b02a8efdbac06a0fede8f6d3019884753d62
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2021-11-16 19:16:20 +0200

    Add files via upload

---
+++ Problema_9 pagina 60.py
@@ -0,0 +1,20 @@
+n=int(input('Dati numarul de elemente din mulțimea A n='))
+A=set()
+print('Introduceti ',n,' elemente pentru mulțimea creată')
+for i in range(0, n):
+    x=str(input('elementul A[' +str(i)+']='))
+    A.add(x)
+print('Mulțimea obtinută',A)
+
+z=int(input('Dati numarul de elemente din mulțimea B n='))
+B=set()
+print('Introduceti ',n,' elemente pentru mulțimea creată')
+for i in range(0, n):
+    y=str(input('elementul B[' +str(i)+']='))
+    B.add(y)
+print('Mulțimea obtinută',B)
+
+print('Intersecția mulțimilor: ', A.intersection(B))
+print('Reuniunea mulțimilor: ', A.union(B))
+print('Diferența mulțimilor: ', A.difference(B))
+
