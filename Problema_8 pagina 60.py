5bb718a315aabf8885887d9524c8eea9e3a395d2
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2021-11-16 19:12:54 +0200

    Delete Problema_8 pagina 60.py

--- Problema_8 pagina 60.py
+++
@@ -1,8 +0,0 @@
-A = {1,4,6,8}
-B = {3,4,2,6}
-print('Intersecția mulțimilor: ', A.intersection(B))
-print('Reuniunea mulțimilor: ', A.union(B))
-print('Diferența mulțimilor: ', A.difference(B))
-        
-
-
